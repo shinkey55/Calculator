565566590311f32f480fd29edaeab6c70ff9f7d1
shinkey55 <shinshin.ak@gmail.com>
2019-12-28 14:31:59 +0900

    電卓のボタン色設定

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.calculator
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var fomula: TextView
+    private var nStr: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fomula = findViewById(R.id.fomula)
+
+        findViewById<Button>(R.id.zero).setOnClickListener {
+            fomula.text = "${fomula.text}0"
+            nStr += 0
+        }
     }
+
 }
